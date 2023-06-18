810697053490feb2e5912466e3de5e21ee16a74e
lkjj-lkjj <83354895+lkjj-lkjj@users.noreply.github.com>
2023-06-18 13:21:42 +0800

    Delete Homework2 directory

--- Homework2/transport-backend/src/main/java/com/example/transport/service/ContractService.java
+++
@@ -1,9 +0,0 @@
-package com.example.transport.service;
-
-import com.example.transport.entity.Contract;
-
-import java.util.List;
-
-public interface ContractService {
-    List<Contract> findAllContracts();
-}
